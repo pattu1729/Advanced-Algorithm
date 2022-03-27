5d9d0ac5bd6542e56adda34df9cd666d705cb839
Prathiban Vinayagamoorthy <60544331+pattu1729@users.noreply.github.com>
2022-03-27 11:33:21 +0530

    Add files via upload

---
+++ depthfirstsearch.cpp
@@ -0,0 +1,172 @@
+#include<stdio.h> 
+
+#include<stdlib.h> 
+
+ 
+
+typedef struct node 
+
+{ 
+
+ struct node *next; 
+
+ int vertex; 
+
+}node; 
+
+ 
+
+node *G[20]; 
+
+int visited[20]; 
+
+int n; 
+
+void reading_graph(); 
+
+void insert(int,int); 
+
+void DFS(int); 
+
+ 
+
+int main() 
+
+{ 
+
+ printf("\n\t\t\t DEPTH FIRST SEARCH\n"); 
+
+ printf("This program is done by JAYA KUMAR.V (19MID0059)\n"); 
+
+ int i; 
+
+
+ read_graph(); 
+
+ for(i=0;i<n;i++) 
+
+ visited[i]=0; 
+
+ DFS(0); 
+
+} 
+
+ 
+
+void DFS(int i) 
+
+{ 
+
+ node *p; 
+
+ printf("%d, ",i); 
+
+ p=G[i]; 
+
+ visited[i]=1; 
+
+ while(p!=NULL) 
+
+ { 
+
+ i=p->vertex; 
+
+ 
+
+ if(!visited[i]) 
+
+ DFS(i); 
+
+ p=p->next; 
+
+ } 
+
+} 
+
+ 
+
+void read_graph() 
+
+{ 
+
+ int i,Vx,Vy,no_of_edges; 
+
+ printf("Enter number of vertices:"); 
+
+ scanf("%d",&n); 
+
+
+ for(i=0;i<n;i++) 
+
+ { 
+
+ G[i]=NULL; 
+
+ printf("Enter number of edges:"); 
+
+ scanf("%d",&no_of_edges); 
+
+ 
+
+ for(i=0;i<no_of_edges;i++) 
+
+ { 
+
+ printf("Enter an edge(x,y):"); 
+
+ scanf("%d%d",&Vx,&Vy); 
+
+ insert(Vx,Vy); 
+
+ } 
+
+ } 
+
+ printf("\nThe depth first search traversal order of given graph---> "); 
+
+} 
+
+ 
+
+void insert(int Vx,int Vy) 
+
+{ 
+
+ node *p,*q; 
+
+ q=(node*)malloc(sizeof(node)); 
+
+ q->vertex=Vy; 
+
+ q->next=NULL; 
+
+ if(G[Vx]==NULL) 
+
+ G[Vx]=q; 
+
+ else 
+
+ { 
+
+ p=G[Vx]; 
+
+
+ 
+
+ while(p->next!=NULL) 
+
+ p=p->next; 
+
+ p->next=q; 
+
+ } 
+
+ 
+
+} 
+
+
+
+ 
+
+
